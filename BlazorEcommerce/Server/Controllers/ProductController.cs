fd75b5399e9bc0b37a639cb093a1268dfe525602
Jerry Sparkman <jerry.sparkman@ssigroup.com>
2022-05-17 12:00:07 -0500

    Added Categories

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -30,5 +30,14 @@ namespace BlazorEcommerce.Server.Controllers
 			return Ok(result);
 
 		}
+
+		[HttpGet("category/{categoryUrl}")]
+		public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductByCategory(string categoryUrl)
+		{
+
+			var result = await _productService.GetProductsByCategory(categoryUrl);
+			return Ok(result);
+
+		}
 	}
 }
